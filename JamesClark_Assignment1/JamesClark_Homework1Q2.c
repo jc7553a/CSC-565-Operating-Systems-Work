d389bdffc2e9150f23f61bd6e8c8e09c875490b4
JP <jc7553a@student.american.edu>
2017-05-04 18:21:10 -0400

    Added Assignment 1

---
+++ JamesClark_Assignment1/JamesClark_Homework1Q2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+/* Main*/
+
+int main()
+{
+int PID = fork();
+int parentPID;
+int parentPPID;
+char * const parmList [] = {"ps"};
+
+/*Error Handling For Child Creation*/
+if (PID == -1)
+	{
+	printf("Error");
+	}
+	
+/*Child Process Code*/
+if (PID==0)
+	{
+	int myPID = getpid();
+	int myPPID = getppid();
+	printf("My Pid = %d my parent PID = %d\n", myPID, myPPID);
+	execl("/bin/ps", "ps", 0,0);
+	}
+
+/*Parents Code*/
+if (PID > 0)
+	{
+	wait(0);
+	parentPID = getpid();
+	parentPPID = getppid();
+	printf("\nThe Child Process has finished\n");
+	printf("Child Pid = %d, my PID = %d, parent PID = %d\n", PID, parentPID, parentPPID);
+	}
+
+return 0;
+}//end program
---
+++ JamesClark_Assignment1/JamesClark_Homework1Q3.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#define BUFFER_SIZE  25
+
+int main(void)
+{
+
+/*CREATE FILE DESCRIPTORS*/
+int fd[2];
+int fd2[2];
+
+/*CREATE STRINGS AND BUFFER*/
+const char argToPass[BUFFER_SIZE] = "Hello";
+char buffer[BUFFER_SIZE];
+const char reverseArg[BUFFER_SIZE] = "hELLO";
+char buffer2[BUFFER_SIZE];
+
+/*CREATE PIPES*/
+int ret = pipe(fd);
+int ret2 = pipe(fd2);
+/*CREATE FORK*/
+int pid;
+pid = fork();
+
+/*ERROR HANDLING FOR PIPES*/
+if (ret == -1 | ret2 == -1)
+{
+	printf("Pipes Failed");
+
+}
+
+/*ERROR HANDLING FOR CHILD CREATION*/
+if(pid < 0)
+{
+	printf("Child Creation Failed!");
+}
+
+/* CHILD PROCESS CODE TO SEND INITIAL Hello AND RECIEVE REVERSE hELLO */
+if (pid == 0)
+{
+	close(fd[0]);
+	write(fd[1], argToPass, strlen(argToPass)+1);
+	close(fd[1]);
+	wait();
+	close(fd2[1]);
+	read(fd2[0], buffer2, BUFFER_SIZE);
+	close(fd[0]);
+	printf("Reverse: %s   ", buffer2);
+	
+}// end pid == 0
+
+
+/*PARENT PROCESS CODE TO RECIEVE INTIAIL Hello  AND SEND REVERSE hELLO*/
+if(pid > 0)
+{
+	close(fd[1]);
+	read(fd[0], buffer, BUFFER_SIZE);
+	close(fd[0]);
+	printf("Initial: %s\n", buffer);
+	printf("\n");
+	close(fd[0]);
+	write(fd2[1], reverseArg, strlen(reverseArg)+1);
+}
+
+return 0;
+}// end program
